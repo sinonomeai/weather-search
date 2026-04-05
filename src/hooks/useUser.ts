b5f78517a08c70a5166ffdcd6a0ed612513259e1
shinanomeai <2534127934@qq.com>
2026-04-05 17:05:47 +0800

    update

--- src/hooks/useUser.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { getUser } from "../api/User"
-export const useUser = (userName:string)=>{
-    return useQuery({
-            queryKey: ["User", userName],
-            queryFn: () => getUser(userName),
-            enabled:false
-        })
-}
--- src/stores/cityName.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand"
-export const useCityData = create((set) => ({
-    cityData: "北京",
-    setCityData: (data: string) => {
-        set({ cityData: data })
-    },
-}))
