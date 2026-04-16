4b61a47ce4d1df24495b04240945492ce221c4e2
shinanomeai <2534127934@qq.com>
2026-04-16 10:31:24 +0800

    message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import fs from "fs";
 import path from "path";
@@ -19,13 +19,13 @@ const writeData = (data: any): void => {
   fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
 };
 
-// 添加类型注解 Request, Response
-app.get("/users", (req: Request, res: Response) => {
+// 移除类型注解，TypeScript 会自动推断
+app.get("/users", (req, res) => {
   const db = readData();
   res.json(db.users);
 });
 
-app.get("/users/:id", (req: Request, res: Response) => {
+app.get("/users/:id", (req, res) => {
   const db = readData();
   const user = db.users.find((u: any) => u.id === req.params.id);
   if (!user) {
@@ -34,7 +34,7 @@ app.get("/users/:id", (req: Request, res: Response) => {
   res.json(user);
 });
 
-app.patch("/users/:id", (req: Request, res: Response) => {
+app.patch("/users/:id", (req, res) => {
   const db = readData();
   const userIndex = db.users.findIndex((u: any) => u.id === req.params.id);
 
